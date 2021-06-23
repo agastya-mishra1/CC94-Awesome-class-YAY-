4a12d28c27cf940671ab587c490f4c8fd5bfde32
agastya-mishra1 <78002464+agastya-mishra1@users.noreply.github.com>
2021-06-22 21:55:07 -0400

    Add files via upload

---
+++ pet_room.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDZflkP3XkpfUopvmPGmWfBKwpdLOtLLQI",
+    authDomain: "pet-connect-de41f.firebaseapp.com",
+    projectId: "pet-connect-de41f",
+    storageBucket: "pet-connect-de41f.appspot.com",
+    messagingSenderId: "629633589381",
+    appId: "1:629633589381:web:d73a180192c6ee2afb89ec"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
